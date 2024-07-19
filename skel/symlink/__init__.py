3ca638c7b1c247c529868248cf6615b247876d1b
Bryan Irvine <sparctacus@gmail.com>
2024-07-19 10:06:39 -0500

    Update __init__.py
    Testing ZPLLoader

--- skel/symlink/__init__.py
+++ skel/symlink/__init__.py
@@ -1,42 +1,25 @@
 import os
-from ZenPacks.zenoss.ZenPackLib import zenpacklib
-
-CFG = zenpacklib.load_yaml([os.path.join(os.path.dirname(__file__), "zenpack.yaml")], verbose=False, level=30)
-schema = CFG.zenpack_module.schema
-
-
-import Globals
-import os.path
 
 from Products.ZenModel.ZenPack import ZenPack as ZenPackBase
 from Products.ZenUtils.Utils import zenPath
 
-#unused(Globals)
-
-
-
 class ZenPack(ZenPackBase):
 
-    def install(self, app):
-        super(ZenPack, self).install(app)
-        self.symlink_plugins()
+    def remove(self, app, leaveObjects=False):
+        '''
+        the script is automatically installed by the ZPLBin loader, but
+        needs to be manually cleaned up on removal. 
+        '''
+        self.removeSymlinks()
+        super(ZenPack, self).remove(app, leaveObjects)
 
-    def symlink_plugins(self):
-        libexec = os.path.join(os.environ.get('ZENHOME'), 'libexec')
-        if not os.path.isdir(libexec):
-            # Stack installs might not have a $ZENHOME/libexec directory.
-            os.mkdir(libexec)
-
-        _plugins = os.listdir(self.path('libexec'))
-        _plugins = [item for item in _plugins if '__init__.py' not in item]
-
-        for plugin in _plugins:
-            #LOG.info('Linking %s plugin into $ZENHOME/libexec/', plugin)
-            plugin_path = zenPath('libexec', plugin)
-            os.system('ln -sf "%s/%s" "%s"' % (self.path('libexec'), plugin, plugin_path))
-            os.system('chmod 0755 %s' % plugin_path)
-
-    def remove_plugin_symlinks(self):
-        for plugin in _plugins:
-            #LOG.info('Removing %s link from $ZENHOME/libexec/', plugin)
-            os.system('rm -f "%s"' % zenPath('libexec', plugin))
+    def removeSymlinks(self):
+        '''
+        Remove files we installed in bin/
+        '''
+        scriptDir = os.path.join(self.path(), "bin")
+        for symLink in [os.path.join(zenPath("bin"), f) for f in os.listdir(scriptDir)]:
+            try:
+                os.unlink(symLink)
+            except OSError:
+                pass
